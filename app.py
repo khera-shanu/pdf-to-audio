fbd94d7fbf257a6c63cb1210fcc21a91347f7845
Shanu Khera <kherashanu@gmail.com>
2021-05-30 14:57:23 +0530

    black format

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import pyttsx3
 from pdfreader import SimplePDFViewer
 
+
 def convert_to_audio(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
 
 with open("/Users/lawliet/pdf-to-audio/pdfs/sample_1.pdf", "rb") as fd:
     viewer = SimplePDFViewer(fd)
